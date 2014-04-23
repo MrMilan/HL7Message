fdac0e7934958a03f4a6a4317f1476aaf88259c1
Milan Polacek <polacek.emilan@gmail.com>
2014-04-23 14:06:19 +0200

    Pridani tridy

---
+++ HL7Message/lineNameValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HL7Message
+{
+    class lineNameValues
+    {
+        public double[] hodnotas;
+        public string nameOfSeries;
+    }
+}
